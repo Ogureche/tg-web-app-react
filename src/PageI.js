e5d9a863f967c39d956af15343602c8f191580d1
Ogureche <125202139+Ogureche@users.noreply.github.com>
2024-02-19 10:05:32 +0300

    2

--- src/PageI.js
+++ src/PageI.js
@@ -1,26 +1,29 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "./PageI.css";
-
-const PageI = ({ showButtons }) => {
-  return (
-    <div>
-      <h1>Страница 1</h1>
-      {/* Содержимое первой страницы */}
-      
-      {/* Условный рендеринг кнопок */}
-      {showButtons && (
-        <>
-          <Link to="/страница-1">
-            <button>Открыть Страницу 1</button>
-          </Link>
-          <Link to="/вторая-страница">
-            <button>Открыть Вторую Страницу</button>
-          </Link>
-        </>
-      )}
-    </div>
-  );
-};
-
+import React from "react"; 
+import { Link } from "react-router-dom"; 
+import "./PageI.css"; 
+ 
+const PageI = ({ showButtons }) => { 
+  return ( 
+    <div> 
+      <div className="a1"> 
+        <img src="./image/iptip.svg" className="a2"/> 
+      </div> 
+      <h1>Страница 1</h1> 
+      {/* Содержимое первой страницы */} 
+       
+      {/* Условный рендеринг кнопок */} 
+      {showButtons && ( 
+        <> 
+          <Link to="/страница-1"> 
+            <button>Открыть Страницу 1</button> 
+          </Link> 
+          <Link to="/вторая-страница"> 
+            <button>Открыть Вторую Страницу</button> 
+          </Link> 
+        </> 
+      )} 
+    </div> 
+  ); 
+}; 
+ 
 export default PageI;
