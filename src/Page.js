73780d84a272119ead61d25601ed94d4faefdbfe
laff1ck <lafficktop@gmail.com>
2024-02-24 12:56:35 +0300

    2

--- src/Page.js
+++ src/Page.js
@@ -4,9 +4,16 @@ import "./Page.css";
 
 const Page = () => {
   return (
+    <div className="main">
+      <div className="c1">
+       <p>Траектория обучения </p> 
+      </div>
+
+    
     <div>
       <h2>Вторая страница</h2>
       <p>Текст для второй страницы</p>
+    </div>
     </div>
   );
 };
