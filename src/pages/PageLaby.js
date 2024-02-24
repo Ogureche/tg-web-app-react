6016fc0398b695ea793246308112718819336e1f
laff1ck <lafficktop@gmail.com>
2024-02-24 14:57:01 +0300

    Update PageLaby.js

---
+++ src/pages/PageLaby.js
@@ -0,0 +1,14 @@
+import React from "react"; 
+import { Link } from "react-router-dom"; 
+import "../styles/PageI.css"; 
+const PageLaby = () => {
+    
+    return (
+        <div className="main">
+            
+        </div>
+
+        );
+    };
+    
+    export default PageLaby;       
