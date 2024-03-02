ba5cc5918767c4c29bbb3289c6c0bf23e5247d6c
karazi2 <yaroslav2antonov@yandex.ru>
2024-03-02 14:31:53 +0300

    3

--- src/components/Hacatons/Hacaton/Hacaton.jsx
+++ src/components/Hacatons/Hacaton/Hacaton.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import s from '../PageHaha.module.css'
+
 const Hacaton = props => {
 	return (
 		<div className={s.hacaton}>
@@ -7,7 +8,15 @@ const Hacaton = props => {
 				<p className={s.main_text}>{props.name_text}</p>
 			</div>
 			<div className={s.description}>
+				<p className={s.info}>{props.info_data}</p>
 				<p className={s.info}>{props.info_text}</p>
+				<p className={s.info}>{props.info_sum}</p>
+			</div>
+
+			<div className={s.link}>
+				<a href={props.link} target='_blank' rel='noopener noreferrer'>
+					<button className={s.button}>Записаться</button>
+				</a>
 			</div>
 		</div>
 	)
