96309a35f01b80ea9d2b7279dad44c9f4ebdd15d
karazi2 <yaroslav2antonov@yandex.ru>
2024-03-03 17:14:52 +0300

    Update useTelegram.js

--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -1,14 +1,30 @@
 const tg = window.Telegram.WebApp
 
 export function useTelegram() {
+	const onClose = () => {
+		tg.close()
+	}
 	var BackButton = tg.BackButton
 	BackButton.show()
 	BackButton.onClick(function () {
 		BackButton.hide()
+	})
+	tg.onEvent('backButtonClicked', function () {
 		window.history.back()
 	})
 
+	const onToggleButton = () => {
+		if (tg.MainButton.isVisible) {
+			tg.MainButton.hide()
+		} else {
+			tg.MainButton.show()
+		}
+	}
+
 	return {
-		BackButton,
+		onClose,
+		onToggleButton,
+		tg,
+		user: tg.initDataUnsafe?.user,
 	}
 }
